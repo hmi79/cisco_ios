fb4ef89f45781e6bc7a5a3a848b119b7756693de
hmi79 <hhami@protonmail.com>
2021-11-20 12:04:05 +0330

    t2est

--- temp-config/b.sh
+++ temp-config/b.sh
@@ -4,7 +4,6 @@ sed -i "s/interface/  - IntName:/g" $hostname
 sed -i "s/ switchport mode access/    IntTrunk: false/g" $hostname
 sed -i "s/ switchport mode trunk/    IntTrunk: true/g" $hostname
 sed -i '/Vlan/d' $hostname
-#cat $hostname >> $hostname2
 cat $hostname | grep -B 1 'IntTrunk' | tee $hostname2
 sed -i '1iSwInterfaces:' $hostname2
 sed -i '1i---' $hostname2
