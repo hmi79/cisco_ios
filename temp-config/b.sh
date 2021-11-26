d14d07cb0aef1109592a185379220f50b4d7ea77
hmi79 <hhami@protonmail.com>
2021-11-26 19:21:16 +0330

    test

--- temp-config/b.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-cat $invname | grep -E 'interface|mode trunk|mode access' | tee $invname
-sed -i "s/interface/  - IntName:/g" $invname
-sed -i "s/ switchport mode access/    IntTrunk: false/g" $invname
-sed -i "s/ switchport mode trunk/    IntTrunk: true/g" $invname
-sed -i '/Vlan/d' $invname
-cat $invname | grep -B 1 'IntTrunk' | tee $invname2
-sed -i '1iSwInterfaces:' $invname2
-sed -i '1i---' $invname2
--- temp-config/b2.sh
+++
@@ -1,3 +0,0 @@
-cat /dev/null > $invname
-cat /dev/null > $invname2
-echo -e "---\nSwInterfaces:"> $invname2
